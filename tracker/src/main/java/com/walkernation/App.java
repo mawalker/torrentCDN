4eeeba09226fc56e4b54f66ed1240c98fa5c3e8c
walker <walker@ubuntu.ubuntu-domain>
2013-11-21 04:57:59 -0600

    separation of tracker and client apps into two different projects, and skeleton vagrent file stuff

---
+++ tracker/src/main/java/com/walkernation/App.java
@@ -0,0 +1,16 @@
+package com.walkernation;
+
+import com.turn.ttorrent.tracker.Tracker;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	Tracker tr;
+        System.out.println( "Hello World!" );
+    }
+}
